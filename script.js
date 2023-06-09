d4bc315ed141fdc3c67743837135123abe9e1d03
Seerat-Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-06-09 12:57:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,41 @@ let btn = document.getElementById("btn");
 let mountains_front = document.getElementById("mountains_front");
 let header = document.querySelector("header");
 
-window.addEventListener('scroll',function(){
-    let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
-    mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
-    header.style.top = value * 0.5 + 'px';
-})
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + "px";
+  moon.style.top = value * 1.05 + "px";
+  mountains_behind.style.top = value * 0.5 + "px";
+  mountains_front.style.top = value * 0 + "px";
+  text.style.marginRight = value * 4 + "px";
+  text.style.marginTop = value * 1.5 + "px";
+  btn.style.marginTop = value * 1.5 + "px";
+  header.style.top = value * 0.5 + "px";
+});
+
+
+//Drop-down menu
+
+let line = document.getElementById("bar");
+let tabs = document.getElementById("tabs");
+let ulli = document.getElementById("ulli");
+function toggleopacity() {
+  if (tabs.style.opacity === 1) {
+    tabs.style.opacity = 0;
+  } else {
+    tabs.style.opacity = 1;
+  }
+}
+
+function togglevisibility() {
+  if (tabs.style.visibility === "hidden") {
+    tabs.style.visibility = "visible";
+  } else {
+    tabs.style.visibility = "hidden";
+  }
+}
+
+line.addEventListener("click", () => {
+  toggleopacity();
+  togglevisibility();
+});
